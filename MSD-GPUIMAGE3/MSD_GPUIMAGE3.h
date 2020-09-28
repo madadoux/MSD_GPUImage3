e31478b2e8f3ff932683aafa9eec6cd70fcb8266
Mohamed saeed <mohedsh@gmail.com>
2020-09-28 16:16:03 +0200

    Initial Commit

---
+++ MSD-GPUIMAGE3/MSD_GPUIMAGE3.h
@@ -0,0 +1,19 @@
+//
+//  MSD_GPUIMAGE3.h
+//  MSD-GPUIMAGE3
+//
+//  Created by Mohamed saeed on 9/28/20.
+//  Copyright © 2020 Mohamed saeed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MSD_GPUIMAGE3.
+FOUNDATION_EXPORT double MSD_GPUIMAGE3VersionNumber;
+
+//! Project version string for MSD_GPUIMAGE3.
+FOUNDATION_EXPORT const unsigned char MSD_GPUIMAGE3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSD_GPUIMAGE3/PublicHeader.h>
+
+
